36a9d0b35f4b835dd6c0e06d9a34972a64ff9bbc
Franco Herrera <franncoherrera2011@gmail.com>
2025-01-05 19:14:48 -0300

    bugfix: hashRouter added for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { GiftExpertApp } from "./gitExpertApp/GifExpertApp.jsx";
 import { HomeApp } from "./homeApp.jsx";
 import "./styles.css";
@@ -8,13 +8,13 @@ import { TodoApp } from "./todoApp/TodoApp.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomeApp />} />
         <Route path="/gift" element={<GiftExpertApp />} />
         <Route path="/todo" element={<TodoApp />} />
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
